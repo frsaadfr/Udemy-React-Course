8b61afcd853d31297bb39aaae991beff1423da27
Saad <saadbinosama@icloud.com>
2023-03-30 02:55:22 -0700

    1st Challenge

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -2,11 +2,44 @@
 
 console.log('APP.js is runninng');
 
+// Real Code 
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'This is some list item'
+        )
+    )
 );
+
+//challenge code
+
+// *completed :]
+
+/*
+var templateTwo = 
+<div>
+    <h1>Saad</h1>
+    <p>16</p>
+    <p>Location:Pakistan</p>
+</div> */
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 console.log('APP.js is runninng')
 
-var template = <h1>Indecision App</h1>;
+// Real Code 
+
+ var template = 
+<div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+
+    <ol>
+        <li>This is some list item</li>
+    </ol>
+</div>; 
+
+
+                        //challenge code
+
+                        // *completed :]
+
+/*
+var templateTwo = 
+<div>
+    <h1>Saad</h1>
+    <p>16</p>
+    <p>Location:Pakistan</p>
+</div> */
+
+
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
