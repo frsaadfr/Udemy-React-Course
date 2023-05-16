7495a8ad2f6422e2613b8e10802ca82dfb3b7f04
Saad <saadbinosama@icloud.com>
2023-05-16 10:42:24 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom'
-import IndesicionApp from './components/IndesicionApp';
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-ReactDOM.render(<IndesicionApp />, document.getElementById('app'));
+
+const ExpenseDashBoardPage = () => (
+    <div>
+    this from my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+    this from my addexpense component
+    </div>
+)
+const EditExpensePage = () => (
+    <div>
+    this from my EditExpensePage component
+    </div>
+)
+const HelpPage = () => (
+    <div>
+    this from my HelpPage component
+    </div>
+)
+const NotFoundPage = () => (
+    <div>
+    404
+    </div>
+)
+
+const routes = (
+    <BrowserRouter>
+    <Switch>
+        
+        <Route path="/" component={ExpenseDashBoardPage} exact={true}/>
+         <Route path="/create" component={AddExpensePage}/>
+         <Route path="/editExpense" component={EditExpensePage} />
+         <Route path="/Help" component={HelpPage} />
+         <Route component={NotFoundPage} />
+    </Switch>
+     
+
+    </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById('app'));
 
